4d968b1b866dd7c5cdc688b1f33789ff1e0678e2
blacktip <blacktip@yellowknife.io>
2023-11-29 13:43:30 +0900

    [VELAU-284] meta-yellowknife 브런치 변경 main -> 2023

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ cd poky
 git clone -b kirkstone git://git.openembedded.org/meta-openembedded
 git clone -b kirkstone https://github.com/meta-qt5/meta-qt5
 git clone -b kirkstone git://git.yoctoproject.org/meta-raspberrypi
-git clone -b main git@github.com:DevYellowknife/meta-yellowknife.git
+git clone -b 2023 git@github.com:DevYellowknife/meta-yellowknife.git
 
 source oe-init-build-env ../build
 
